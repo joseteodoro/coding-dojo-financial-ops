c55b47acbf8632f6a3b2c89b014488b3084468cc
Flavio <flavio@flavio.andrade>
2022-01-26 15:03:50 -0300

    tdd

--- src/wallet.ts
+++ src/wallet.ts
@@ -1,1 +1,22 @@
+type MessagensProps = {
+    type: string,
+    owner: string,
+    balance: number,
+} 
 
+export default class Wallet{
+    balance: number 
+    constructor({type, owner, balance }:MessagensProps){
+        this.balance = balance 
+        
+    }
+    
+    static of(messages:MessagensProps[]): Wallet{
+       
+       if (messages[0].type === "create") {
+          return new Wallet({balance:0, ...messages[0]})
+          
+       } 
+       return null
+    }
+}
