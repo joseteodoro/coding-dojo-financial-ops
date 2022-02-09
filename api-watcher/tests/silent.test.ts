2ecddd6d03034d49a358f41212a425418c542e4b
jose teodoro da silva <jose.silva@remessaonline.com.br>
2022-02-09 14:12:05 -0300

    removing unnecessary code

---
+++ api-watcher/tests/silent.test.ts
@@ -0,0 +1,6 @@
+describe('wallet test suite', () => {
+    describe("give a non existent wallet", () => {
+        it('simple wallet creation should return balance zero', () => {
+        })
+    })
+})
