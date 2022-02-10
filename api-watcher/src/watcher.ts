d3d767d36fd42a0e4de0c0d37963021f877c6217
daeppe <d.pena@hotmail.com>
2022-02-10 17:28:11 -0300

    feat: Implementaçao do watcher, 4 testes passaram.

--- api-watcher/src/watcher.ts
+++ api-watcher/src/watcher.ts
@@ -1,10 +1,18 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosResponse, AxiosError } from 'axios';
 
 const evaluateAll = async (items: any[]): Promise<any[]> => {
     return Promise.all(items.map((item) => {
         return axios(item, { validateStatus: () => true })
-            .then((data: AxiosResponse) => data.status)
-            .catch(err => err.response.status)
+            .then((data: AxiosResponse) => {
+                return {status: data.status}
+            })
+            .catch((err: AxiosError) => {
+                // console.log(err)
+                return { 
+                    error: "Could not resolve host",
+                    status: err.response?.status
+                }
+            })
     }))
 }
 
