b734ec4584f50777da75a964dc492c49506dc693
jose teodoro da silva <jose.silva@remessaonline.com.br>
2022-02-10 17:13:19 -0300

    adding status and error on tests

--- api-watcher/tests/watcher.test.ts
+++ api-watcher/tests/watcher.test.ts
@@ -9,6 +9,10 @@ describe('watcher test suite', () => {
 
     it('return 404 for inexistant url and 200 for a valid url', async () => {
         const results = await watcher.evaluateAll(['https://stackoverflow.com/non-existent', 'http://google.com'])
-        expect(results).to.be.deep.equal([404, 200])
+        expect(results).to.be.deep.equal([{status: 404}, {status: 200}])
+    })
+    it('return dns error when hit an unreacheable server', async () => {
+        const results = await watcher.evaluateAll(['https://hajskhdfkjashdjkfahsd.com', 'http://google.com'])
+        expect(results).to.be.deep.equal([{status: undefined, error: 'Could not resolve host'}, {status: 200}])
     })
 })
